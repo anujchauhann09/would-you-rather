25051fa35ab25c271a0541f4af04821c6edc776d
Anuj Chauhan <166821850+anujchauhann09@users.noreply.github.com>
2024-08-19 06:58:27 +0000

    Implement Login component with predefined user selection

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,7 +6,7 @@ export default function Login() {
     const [users, setUsers] = React.useState({})
 
     useEffect(() => {
-        const response = _getUsers()
+        const response = _getUsers() 
         response.then(result => {
             setUsers(result)
         }).catch(error => console.log(error))
@@ -17,29 +17,29 @@ export default function Login() {
     // }, [users])
 
     return (
-        <div className="login-container w-3/4 max-w-[480px] sm:max-w-[640px] bg-slate-600 mx-auto mt-16 sm:mt-10">
-            <div className="login-header p-2 bg-teal-600">
-                <span className="text-xl text-white">Login</span>
+        <div className="login-container shadow-lg rounded-md w-11/12 max-w-[480px] sm:max-w-[640px] bg-slate-600 mx-auto mt-16 sm:mt-10">
+        <div className="login-header p-2 bg-teal-600">
+            <span className="text-xl text-white">Login</span>
+        </div>
+        <div className="login-main bg-slate-50 p-4 flex flex-col gap-3 w-full">
+            <div className="user-image-box mx-auto">
+            <img src="/images/johndoe.png" alt="User" className="rounded-full h-40 w-40 sm:h-48 sm:w-48 shadow-lg" />
             </div>
-            <div className="login-main bg-slate-50 p-4 flex flex-col gap-3 w-full">
-                <div className="user-image-box">
-                    <img src="/images/johndoe.png" alt="image" />
-                </div>
-                <div className="user-name flex justify-center">
-                    <select id="user-select" className="p-2 w-1/2">
-                    <option value="Select User">Select User</option>
-                    {
-                        Object.keys(users).map(key => {
-                            const user = users[key]
+            <div className="user-name flex justify-center">
+            <select id="user-select" className="p-2 w-3/4 sm:w-1/2 border border-gray-300 rounded">
+                <option value="Select User">Select User</option>
+                {Object.keys(users).map((key) => {
+                const user = users[key];
 
-                            return (
-                                <option key={user.id} value={user.name}>{user.name}</option>
-                            )
-                        })
-                    }
-                    </select>
-                </div>
+                return (
+                    <option key={user.id} value={user.name}>
+                    {user.name}
+                    </option>
+                );
+                })}
+            </select>
             </div>
+        </div>
         </div>
     )
 }
